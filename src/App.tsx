4de293496b0ffed99a5c8d2d7f4116a26acf6bf7
36t <saluta@gmail.com>
2020-12-13 14:25:44 +0900

    feat: added Fetch component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Counter } from 'features/counter/Counter'
 import TaskList from 'features/task/TaskList'
 import TaskInput from 'features/task/TaskInput'
+import Fetch from 'features/fetch/Fetch'
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Counter />
         <TaskList />
         <TaskInput />
+        <Fetch />
       </header>
     </div>
   )
